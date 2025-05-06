af7b1c2c14d9b95416a61ab615b9bebb757b0620
Rajesh Pal <ajoypal.raj@gmail.com>
2025-05-06 11:56:06 +0600

    framermotion: keyframes like animation example

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,21 @@ const App = () => {
     <div className="bg-black w-full min-h-screen">
       <motion.div
         initial={{
-          x:200,
+          x:0,
+          y:0
         }}
         animate={{
-          x: 1000,
-          rotate:40
+          x: [0,800,800,0,0],
+          y: [0,0,300,300,0],
         }}
         transition={{
           duration: 3,
-          delay: 3,
-          repeat:4,
-          ease:anticipate
+          delay: 2,
+          repeat:2,
         }}
         className="bg-red-400 w-48 h-48 border-4 border-white"
       ></motion.div>
-      <motion.div
-        animate={{
-          x: 1000,
-          scale: 0.5,
-        }}
-        transition={{
-          duration: 3,
-          delay: 3,
-        }}
-        className="bg-amber-400 rounded-full w-48 h-48 border-4 border-white"
-      ></motion.div>
+
     </div>
   );
 };
