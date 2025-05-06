0a649b01a730ad04a86a0aabb7a4ad1ce9da50cb
Rajesh Pal <ajoypal.raj@gmail.com>
2025-05-06 11:28:29 +0600

    framermotion: animate, transition

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { motion } from "motion/react";
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-black w-full min-h-screen">
+      <motion.div
+        animate={{
+          x: 1000,
+          y: 500,
+        }}
+        transition={{
+          duration: 3,
+          delay: 3,
+        }}
+        className="bg-red-400 w-48 h-48 border-4 border-white"
+      ></motion.div>
+      <motion.div
+        animate={{
+          x: 1000,
+          scale: 0.5,
+        }}
+        transition={{
+          duration: 3,
+          delay: 3,
+        }}
+        className="bg-amber-400 rounded-full w-48 h-48 border-4 border-white"
+      ></motion.div>
+    </div>
+  );
+};
+
+export default App;
