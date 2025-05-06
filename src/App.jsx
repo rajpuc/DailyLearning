92aa4890d7803e4894be22239e6f153d3bb865af
Rajesh Pal <ajoypal.raj@gmail.com>
2025-05-06 11:45:10 +0600

    framermotion:  initial,transition[repeat,ease[anticipate]]

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { motion } from "motion/react";
+import { anticipate, motion } from "motion/react";
 import React from "react";
 
 const App = () => {
   return (
     <div className="bg-black w-full min-h-screen">
       <motion.div
+        initial={{
+          x:200,
+        }}
         animate={{
           x: 1000,
-          y: 500,
+          rotate:40
         }}
         transition={{
           duration: 3,
           delay: 3,
+          repeat:4,
+          ease:anticipate
         }}
         className="bg-red-400 w-48 h-48 border-4 border-white"
       ></motion.div>
